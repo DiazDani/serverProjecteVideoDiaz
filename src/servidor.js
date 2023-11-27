9ee8d096db03a6b0b17c0a4e6ae15d2fb6be1323
diazdani <daniel.diaz@institutvidreres.cat>
2023-11-27 10:46:21 +0100

    commit base server

--- src/servidor.js
+++ src/servidor.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const {Server} = require("socket.io")
+const io = new Server(8888)
+const fs = require('fs')
 
 app.use(cors());
 app.use(express.json());
-
 port = 3080;
 
 app.listen(port, ()=>{
   console.log(`el port::${port} funciona`)
 });
+
+io.on('connection', (socket)=>{
+  socket.emit("hello", "world")
+  console.log("cliente connectado")
+
+  let code= nouCodi()
+
+  socket.emit('generarCodigo', this.code)
+
+
+})
+
+function nouCodi(){
+  var chars = 'ABCDEFGHIJKLNMOPQRSTUVWXYZ1234567890'
+  var code= ''
+  var num = 0
+
+  for (let i = 0; i < 5; i++) {
+    num= Math.floor(Math.random() * chars.length+1);
+    code.concat(chars.charAt(num))
+  }
+
+  return code
+}

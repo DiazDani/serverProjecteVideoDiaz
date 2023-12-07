8d204591e39747c69a91fcd07259dcce21fb5174
diazdani <daniel.diaz@institutvidreres.cat>
2023-12-07 14:05:42 +0100

    commit funcionen les validacions

--- src/servidor.js
+++ src/servidor.js
@@ -9,52 +9,46 @@ app.use(cors());
 app.use(express.json());
 port = 3080;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`el port::${port} funciona`)
 });
 
-
-
-io.on('connection', (socket)=>{
+let code
+io.on('connection', (socket) => {
   socket.emit("hello", "world")
   console.log("cliente connectado")
 
 
-  let code
-
-  socket.on('generarCodigo', (args)=>{
-    code= nouCodi()
-
-    console.log(code)
-
-    socket.emit("nouCode", code)
-
+  socket.on('generarCodigo', (args) => {
+    code = nouCodi();
+    console.log(code);
+    socket.join(code); // Unir a la sala con el código generado
+    socket.emit("nouCode", code);
   })
 
-socket.on('EnviarCodiPeli', (newCode)=>{
-  console.log("aaaaa")
-    console.log(newCode)
+  socket.on('EnviarCodiPeli', (clientCode) => {
 
-    if(newCode==code){
-      console.log('true')
-    }else{
-      console.log('false')
+
+    console.log(clientCode);
+    if (clientCode === code) {
+      io.to(code).emit("codigoCorrecto", true);
+    } else {
+      io.to(code).emit("codigoCorrecto", false);
     }
 
   })
 
 
-
 })
 
-function nouCodi(){
+function nouCodi() {
   const chars = 'ABCDEFGHIJKLNMOPQRSTUVWXYZ1234567890'
-  let code= ''
+  let code = ""
   let num
 
   for (let i = 0; i < 5; i++) {
-    num= Math.floor(Math.random() * chars.length+1);
-    code= code + chars.charAt(num)
+    num = Math.floor(Math.random() * chars.length + 1);
+    code = code + chars.charAt(num)
   }
 
   return code

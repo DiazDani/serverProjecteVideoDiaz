708e7ae222916d5cb9a5397b503c2ae06be55807
Pau casas <pau.casas10@gmail.com>
2023-12-11 01:02:49 +0100

    VIDEOS-HECHOS

--- src/servidor.js
+++ src/servidor.js
@@ -1,67 +1,104 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
-const {Server} = require("socket.io")
-const io = new Server(8888)
-const fs = require('fs')
 const path = require('path');
+const fs = require('fs');
+const SocketIOFile = require('socket.io-file');
 
-
+const app = express();
 app.use(cors());
 app.use(express.json());
-port = 3080;
 
-
-
-const videos = [
-  { name: 'Avatar 2', path: 'assets/videos/Avatar 2.mp4', valid: false },
-  { name: 'Guardianes de la galaxia vol 3.mp4', path: 'assets/videos/Guardianes de la galaxia vol 3.mp4', valid: false },
-  { name: 'OPPENHEIMER', path: 'assets/videos/OPPENHEIMER.mp4', valid: false },
-  { name: 'Five Nights At Freddy\'s.mp4', path: 'assets/videos/Five Nights At Freddys.mp4', valid: false },
-]
-
-app.listen(port, () => {
-  console.log(`el port::${port} funciona`)
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST'],
+    credentials: true,
+    transports: ['websocket', 'polling'],
+  },
 });
 
-let code
+const port = 3080;
+
+// Ruta al archivo de contraseñas
+const passwordsPath = path.join(__dirname, 'assets', 'contraseñas.txt');
+
+let passwords = {};
+
+// Lee las contraseñas del archivo y almacénalas en el objeto 'passwords'
+function getPasswords() {
+  const passwordsFile = fs.readFileSync(passwordsPath, 'utf8');
+  const passwordLines = passwordsFile.split('\n');
+
+  passwordLines.forEach((line) => {
+    const [video, password] = line.split(':');
+    passwords[video.trim()] = password.trim();
+  });
+}
+
+// Esta función verifica si la contraseña proporcionada es correcta para el video especificado
+function validatePassword(video, enteredPassword) {
+  return passwords[video] === enteredPassword;
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+let isFirstConnection = true;
+
+const uploader = new SocketIOFile(io, {
+  uploadDir: 'uploads',
+  transmissionDelay: 0,
+  overwrite: true,
+});
+
+uploader.on('start', (fileInfo) => {
+  console.log('Iniciando transferencia de archivo:', fileInfo);
+});
+
+uploader.on('end', (fileInfo) => {
+  console.log('Transferencia de archivo completada:', fileInfo);
+
+  io.emit('file-received', {
+    data: fs.readFileSync(fileInfo.path).toString('base64'),
+    type: fileInfo.type,
+  });
+});
+
+// Videos disponibles para reproducir
+const videos = {
+  video1: 'C:\\Users\\Joan Casas\\IdeaProjects\\serverProjecteVideoDiaz\\src\\assets\\trailer1.mp4',
+  video2: 'C:\\Users\\Joan Casas\\IdeaProjects\\serverProjecteVideoDiaz\\src\\assets\\onepiecetrailer.mp4',
+  video3: 'C:\\Users\\Joan Casas\\IdeaProjects\\serverProjecteVideoDiaz\\src\\assets\\trailerendgame.mp4',
+  video4: 'C:\\Users\\Joan Casas\\IdeaProjects\\serverProjecteVideoDiaz\\src\\assets\\trailerpiratasdelcaribe.mp4',
+};
+
 io.on('connection', (socket) => {
-  socket.emit("hello", "world")
-  console.log("cliente connectado")
+  console.log('Cliente conectado');
+
+  socket.on('play-video', (videoPath) => {
+    const selectedVideoPath = videos[videoPath];
+    const videoData = fs.readFileSync(selectedVideoPath).toString('base64');
+
+    io.emit('file-received', {
+      data: videoData,
+      type: 'video/mp4',
+    });
+  });
+
+  socket.emit('hello', 'world');
+  console.log('cliente conectado');
 
   socket.emit('listaVideos', videos);
 
-  socket.on('generarCodigo', (args) => {
-    code = nouCodi();
-    console.log(code);
-    socket.join(code); // Unir a la sala con el código generado
-    socket.emit("nouCode", code);
-  })
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
 
-  socket.on('EnviarCodiPeli', (clientCode) => {
-
-
-    console.log(clientCode);
-    if (clientCode === code) {
-      io.to(code).emit("codigoCorrecto", true);
-    } else {
-      io.to(code).emit("codigoCorrecto", false);
-    }
-
-  })
-
-
-})
-
-function nouCodi() {
-  const chars = 'ABCDEFGHIJKLNMOPQRSTUVWXYZ1234567890'
-  let code = ""
-  let num
-
-  for (let i = 0; i < 5; i++) {
-    num = Math.floor(Math.random() * chars.length + 1);
-    code = code + chars.charAt(num)
-  }
-
-  return code
-}
+server.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
